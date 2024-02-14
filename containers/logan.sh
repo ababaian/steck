f17374f18fb0b1ff4990a27fe3a71eacbb000f15
ababaian <artem@rRNA.ca>
2024-02-14 13:30:06 -0500

    Initialize base logan-steck container/script

--- containers/logan.sh
+++ containers/logan.sh
@@ -24,31 +24,34 @@ function usage {
   echo ""
   echo "    Required Fields"
   echo "    -q    Query database file. '.cm' (infernal)"
-  echo "    -f    Fasta search file. S3 bucket path for input contigs [s3://bucket/contigs.fa]"
+  echo "    -f    Fasta search file or S3 path for contig file [s3://bucket/contigs.fa]"
   echo ""
   echo "    Alignment Parameters"
   echo "    -a    Alignment software ['infernal' (default) | 'diamond' (NA) | 'rdrp' (NA) ]"
   echo "    -n    parallel CPU threads to use where applicable  [1]"
   echo ""
   echo "    AWS / S3 Bucket parameters"
+  echo "    -A    Flag. Imports ~/.aws/credentials.csv file for container AWS access."
+  echo "                requires mounting (-v) CSV file at Docker Run (see usage below)"
+  echo "          Not required if using Cloud/IAM"
   echo ""
   echo "    Output options"
   echo "    -v    Flag. Verbose output"
   echo "    -b    Base directory in container [/home/logan/]"
-  echo "    -3    S3 Bucket Directory to output output"
+  echo "    -3    [s3://logan-steck/] S3 Bucket Directory to upload output into"
   echo "    -o    <output_prefix> [Defaults]"
   echo ""
   echo "    Outputs Uploaded to s3: "
   echo "          <output_prefix>.fq.xxxx ... <output_prefix>.fq.yyyy"
   echo ""
-  echo "ex: sudo docker run logan -q dvr5.cm -f mdv.fa -o utest"
+  echo 'ex: sudo docker run logan -v $HOME/.aws/logan:/home/logan/aws.csv -A -q dvr5.cm -f mdv.fa -o utest'
   false
   exit 1
 }
 
 # PARSE INPUT =============================================
 # Generate random alpha-numeric for run-id
-RUNID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1 )
+#RUNID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1 )
 
 # Set Default Parameters
 # Alignment files
@@ -65,9 +68,10 @@ ALIGNER='infernal'
 THREADS='1'
 
 # Options
+AWSCRED='FALSE'
 VERBOSE='FALSE'
 
-while getopts q:f:a:n:b:3:o:vh FLAG; do
+while getopts q:f:a:n:b:3:o:v:Ah FLAG; do
   case $FLAG in
     # Search Files  -----------
     q)
@@ -93,6 +97,9 @@ while getopts q:f:a:n:b:3:o:vh FLAG; do
     3)
       S3=$OPTARG
       ;;
+    A)
+      AWSCRED='TRUE'
+      ;;
     v)
       VERBOSE='TRUE'
       ;;
@@ -106,6 +113,18 @@ while getopts q:f:a:n:b:3:o:vh FLAG; do
   esac
 done
 
+# INIT AWS ================================================
+# When running Docker Container locally, the credentials to
+# use AWS need to be mounted into the container from the
+# local filesystem.
+# Ensure "User Name" field is present and set to `default`
+# in the aws credentials.csv file
+# usage: 
+# docker run logan -v $HOME/.aws/logan:/home/logan/aws.csv
+if [[ $AWSCRED == 'TRUE' ]]
+then
+  aws configure import --csv file:///home/logan/aws.csv
+fi
 
 # RUN INFERNAL ============================================
 echo "=========== LOGAN ==========="
@@ -116,6 +135,20 @@ echo "Tool:   $ALIGNER"
 echo "Thread: $THREADS"
 echo "Output: $OUTNAME"
 echo ""
+
+echo 'Test S3 permissions with ls'
+aws s3 ls s3://lovelywater2/
+
+if [[ $FA == s3://* ]]
+then
+  # NOTE: Consider implementing Mountpoint
+  # https://github.com/awslabs/mountpoint-s3/tree/main
+  echo "S3 FA file provided"
+  aws s3 cp $FA $OUTNAME.fa
+  FA="$OUTNAME.fa"
+else
+  echo "Local FA file provided"
+fi
 
 if [[ $VERBOSE == 'TRUE' ]]
 then 
@@ -138,17 +171,17 @@ else
     $FA
 fi
 
-# If an S3 Bucket is provided, upload the output to S3
-# Else print to STDOUT
-if [ "$S3" == "s3://*" ]
+# If no output S3 Bucket is provided, print to STDOUT
+# Else upload the output to S3 bucket-dir
+if [[ $S3 == '' ]]
 then
+  # Output 
+  cat $OUTNAME.tblout
+else
 	#aws s3 cp --only-show-errors $OUTNAME.tblout $S3
   echo "uploading output:"
   echo "  aws s3 cp $OUTNAME.tblout $S3"
-
 	aws s3 cp $OUTNAME.tblout $S3
-else
-	cat $OUTNAME.tblout
 fi
 
 # RUN UPLOAD ==============================================
